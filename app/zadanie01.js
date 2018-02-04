e6df66985ee5f8f240949bce588ecb1ceb701a22
Jerzy Drobot <jerzydrobot@gmail.com>
2018-02-04 11:38:07 +0100

    Zadanie 01 resolved

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,25 @@
 //Twój kod
+const fs = require('fs');
+
+fs.readFile('./data/zadanie01/input.json', (err, data) => {
+  if (err === null) {
+    let dataParsed = JSON.parse(data);
+    console.log(dataParsed);
+    let sumNumbers = (a,b) => a + b;
+    let sum = dataParsed.reduce(sumNumbers);
+    console.log(sum);
+
+    fs.writeFile('data/zadanie01/sum.txt',sum , err => {
+      if (err === null) {
+        console.log("Save file success!");
+      }
+      else {
+        console.log("Unable save file", err);
+      }
+
+    });
+  }
+  else {
+    console.log("Unable read file", err)
+  }
+})
