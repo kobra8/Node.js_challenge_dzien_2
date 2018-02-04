dc60a9182a58d2fdebc39dc5a0c5a07e8f166d5c
kobra8 <jerzydrobot@gmail.com>
2018-02-04 22:54:00 +0100

    ZadanieDnia resolved

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -6,23 +6,31 @@ console.log(input);
 
 fs.readFile(input, 'utf8', (err, data) => {
 
+  let result = "";
   if(err === null) {
     console.log("Read success");
     console.log(data);
-
+    let count = 0;
     for( let char of data ) {
-      console.log(char);
+      if(count% 2 !== 0) {
+      result+=char;
+      }
+      else {
+        result+=char.toUpperCase();
+      }
+      count ++
     };
+    console.log(result);
     
     
-    // fs.writeFile(input, err => {
-    //   if(err === null) {
-    //     console.log("Success save file!");
-    //   }
-    //   else {
-    //     console.log("Error save", err);
-    //   }
-    // })
+    fs.writeFile('data/zadanieDnia/test.txt', result, err => {
+      if(err === null) {
+        console.log("Success save file!");
+      }
+      else {
+        console.log("Error save", err);
+      }
+    })
 
   }
   else {
